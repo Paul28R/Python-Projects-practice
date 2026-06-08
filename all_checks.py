20170d56fd18e1651d9d2ea3634dab7ba7993fe9
Paul28 <Paul28rodriguez@gmail.com>
2026-06-07 22:55:47 -0400

    Iterate over a list of checks and messages to avoid code duplication

--- all_checks.py
+++ all_checks.py
@@ -25,13 +25,15 @@ def check_root_full():
     """Returns True if the root partition is full, False otherwise."""
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
-def main(): 
-    if check_reboot():
-        print("Pending Reboot.")
-        sys.exit(1)
-    if check_root_full():
-        print("Root partition full.")
-        sys.exit(1)
+def main():
+    checks = [
+        (check_reboot, "Pending Reboot."),
+        (check_root_full, "Root partition full."),
+    ]
+    for check, message in checks:
+        if check():
+            print(message)
+            sys.exit(1)
     
     print("Everything ok")
     sys.exit(0)
